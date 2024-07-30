6a89cb7c3b7718bcbb38d3854a4520476e064b83
Camila <alvescamila87@gmail.com>
2024-07-29 22:24:05 -0300

    create produto get method in controller

--- src/main/java/br/com/camila/naturassp/controllers/ProdutoController.java
+++ src/main/java/br/com/camila/naturassp/controllers/ProdutoController.java
@@ -1,8 +1,23 @@
 package br.com.camila.naturassp.controllers;
 
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.camila.naturassp.dao.ProdutoDAO;
+import br.com.camila.naturassp.model.Produto;
 
 @RestController
 public class ProdutoController {
+	
+	@Autowired
+	ProdutoDAO dao;
+	
+	@GetMapping("/produtos")
+	public ArrayList<Produto> listarTodos(){
+		return (ArrayList<Produto>) dao.findAll(); 
+	}
 
 }
